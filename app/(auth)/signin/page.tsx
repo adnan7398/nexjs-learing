4b3a6a3cca292e50c1c322cee4133d8af131db5f
adnan7398 <123adnansiddiqui@gmail.com>
2024-12-24 20:19:39 +0530

    learn about nextjs group routes

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,8 @@
+export default function(){
+    return <div>
+        Sign in 
+        <input type="text" />
+        <input type="text" />
+        <button>Sign in </button>
+    </div>
+}
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,8 @@
+export default function(){
+    return <div>
+        Sign up 
+        <input type="text" />
+        <input type="text" />
+        <button>Sign up</button>
+    </div>
+}
